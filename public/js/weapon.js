a58d744abaf8723676ae8c8b1f92c0f962f21b4c
Nick Payne <nick@kurai.co.uk>
2011-10-20 10:36:40 +0100

    rejigged weapon reload logic to not rely on timers (refs #521)

--- public/js/weapon.js
+++ public/js/weapon.js
@@ -1,18 +1,25 @@
 Weapon = function() {
     var _rof = 1000,
         _loaded = true,
-        _reloadHandler = null;
+        _firedAt = new Date().getTime();
 
     this.isLoaded = function() {
+        // save ourself some maths if we don't have to
+        if (_loaded) {
+            return true;
+        }
+
+        // otherwise, work it out
+        var now = new Date().getTime();
+        _loaded = (now >= _firedAt + _rof);
+
         return _loaded;
     }
 
     this.fire = function(options) {
         GameManager.spawnBullet(options);
         _loaded = false;
-        _reloadHandler = setTimeout(function() {
-            _loaded = true;
-        }, _rof);
+        _firedAt = new Date().getTime();
     };
 };
 
