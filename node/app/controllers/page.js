3602093dec0d00bf953f21a6d28856fca109d125
Nick Payne <nick@kurai.co.uk>
2011-10-27 20:29:01 +0100

    moving normal route actions into proper page controller (fixes #627)

---
+++ node/app/controllers/page.js
@@ -0,0 +1,126 @@
+var utils   = require('app/shared/utils');
+var db      = require('app/db');
+
+var app = null;
+/**
+ * global view variables (bleuch)
+ */
+var settings = {};
+
+var PageController = {
+    init: function(app) {
+        settings.trackStats = app.enabled('trackStats')
+    },
+
+    index: function(req, res) {
+        res.render('index', {
+            "settings": settings
+        });
+    },
+
+    about: function(req, res) {
+        res.render('about', {
+            'pageTitle': 'About',
+            "settings": settings
+        });
+    },
+
+    topUsers: function(req, res) {
+        db.collection('users', function(err, collection) {
+            collection
+            .find()
+            .limit(100)
+            .sort({rank: -1})
+            .toArray(function(err, docs) {
+                var players = [];
+                res.render('top-users', {
+                    'pageTitle': 'Top 100 Players',
+                    users: docs,
+                    "settings": settings
+                });
+            });
+        });
+    },
+
+    userProfile: function(req, res) {
+        var username = req.params[0];
+        db.collection('users', function(err, collection) {
+            collection.findOne({"username": username}, function(err, user) {
+                if (user == null) {
+                    res.send("Invalid user");
+                    return;
+                }
+                if (!user.shots) {
+                    user.accuracy = 0;
+                } else {
+                    var hits = user.hits || 0;
+                    user.accuracy = Math.round((hits / user.shots)*100);
+                }
+                db.collection('games', function(err, collection) {
+                    collection
+                    .find({isFinished: true, $or : [{"challenger.db_id": user._id}, {"challengee.db_id": user._id}]})
+                    .limit(6)
+                    .sort({started: -1})
+                    .toArray(function(err, docs) {
+
+                        var games = [];
+                        docs.forEach(function(game) {
+                            // we could probably do this far more efficiently, but we need to
+                            // augment the game objects before rendering them
+
+                            game.elapsed = utils.formatTime(
+                                Math.round((game.finished - game.started) / 1000)
+                            );
+
+                            var outcome = "";
+                            if (game.defaulted) {
+                                var defaulter = game.defaulter.toString() == game.challenger.db_id.toString() ?
+                                    game.challenger :
+                                    game.challengee;
+
+                                outcome = "Forfeited by "+defaulter.username;
+                            } else if (game.cancelled) {
+                                outcome = "Cancelled";
+                            } else {
+                                if (game.winner == user._id.toString()) {
+                                    outcome = "Won";
+                                } else {
+                                    outcome = "Lost";
+                                }
+                                if (game.suddendeath) {
+                                    outcome += " (Sudden Death)";
+                                }
+                            }
+
+                            game.outcome = outcome;
+
+                            games.push(game);
+                        });
+                        collection
+                        .find({isFinished: true, $or : [{"challenger.db_id": user._id}, {"challengee.db_id": user._id}]})
+                        .count(function(err, count) {
+                            user.gamesPlayed = count;
+                            db.collection('daily_rankings', function(err, collection) {
+                                collection.findOne({user_id: user._id}, function(err, doc) {
+                                    var stats = [];
+                                    if (doc != null) {
+                                        stats = doc.stats;
+                                    }
+                                    res.render('user', {
+                                        'pageTitle': 'User Profile',
+                                        user: user,
+                                        games: games,
+                                        stats: stats,
+                                        "settings": settings
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    }
+};
+
+module.exports = PageController;
--- node/app/routes.js
+++ node/app/routes.js
@@ -1,134 +1,33 @@
-var utils   = require('./shared/utils');
-var db      = require('./db');
+var PageController = require('app/controllers/page');
 
 module.exports = function(app) {
-    /**
-     * global view variables (bleuch)
-     */
-    var settings = {
-        'trackStats': app.enabled('trackStats')
-    };
-
+    PageController.init(app);
     /**
      * Home Page
      */
     app.get('/', function(req, res) {
-        res.render('index', {
-            "settings": settings
-        });
+        PageController.index(req, res);
     });
 
     /**
      * About Page 
      */
     app.get('/about', function(req, res) {
-        res.render('about', {
-            'pageTitle': 'About',
-            "settings": settings
-        });
+        PageController.about(req, res);
     });
 
     /**
      * Top 100 users
      */
     app.get('/users/top', function(req, res) {
-        db.collection('users', function(err, collection) {
-            collection
-            .find()
-            .limit(100)
-            .sort({rank: -1})
-            .toArray(function(err, docs) {
-                var players = [];
-                res.render('top-users', {
-                    'pageTitle': 'Top 100 Players',
-                    users: docs,
-                    "settings": settings
-                });
-            });
-        });
+        PageController.topUsers(req, res);
     });
 
     /**
      * User profile page
      */
     app.get(/\/user\/([A-z0-9_]+)/, function(req, res) {
-        var username = req.params[0];
-        db.collection('users', function(err, collection) {
-            collection.findOne({"username": username}, function(err, user) {
-                if (user == null) {
-                    res.send("Invalid user");
-                    return;
-                }
-                if (!user.shots) {
-                    user.accuracy = 0;
-                } else {
-                    var hits = user.hits || 0;
-                    user.accuracy = Math.round((hits / user.shots)*100);
-                }
-                db.collection('games', function(err, collection) {
-                    collection
-                    .find({isFinished: true, $or : [{"challenger.db_id": user._id}, {"challengee.db_id": user._id}]})
-                    .limit(6)
-                    .sort({started: -1})
-                    .toArray(function(err, docs) {
-
-                        var games = [];
-                        docs.forEach(function(game) {
-                            // we could probably do this far more efficiently, but we need to
-                            // augment the game objects before rendering them
-
-                            game.elapsed = utils.formatTime(
-                                Math.round((game.finished - game.started) / 1000)
-                            );
-
-                            var outcome = "";
-                            if (game.defaulted) {
-                                var defaulter = game.defaulter.toString() == game.challenger.db_id.toString() ?
-                                    game.challenger :
-                                    game.challengee;
-
-                                outcome = "Forfeited by "+defaulter.username;
-                            } else if (game.cancelled) {
-                                outcome = "Cancelled";
-                            } else {
-                                if (game.winner == user._id.toString()) {
-                                    outcome = "Won";
-                                } else {
-                                    outcome = "Lost";
-                                }
-                                if (game.suddendeath) {
-                                    outcome += " (Sudden Death)";
-                                }
-                            }
-
-                            game.outcome = outcome;
-
-                            games.push(game);
-                        });
-                        collection
-                        .find({isFinished: true, $or : [{"challenger.db_id": user._id}, {"challengee.db_id": user._id}]})
-                        .count(function(err, count) {
-                            user.gamesPlayed = count;
-                            db.collection('daily_rankings', function(err, collection) {
-                                collection.findOne({user_id: user._id}, function(err, doc) {
-                                    var stats = [];
-                                    if (doc != null) {
-                                        stats = doc.stats;
-                                    }
-                                    res.render('user', {
-                                        'pageTitle': 'User Profile',
-                                        user: user,
-                                        games: games,
-                                        stats: stats,
-                                        "settings": settings
-                                    });
-                                });
-                            });
-                        });
-                    });
-                });
-            });
-        });
+        PageController.userProfile(req, res);
     });
 
     /**
